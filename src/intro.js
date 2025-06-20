8e51591103d0cc9e9b567c04db70f229d2553eb1
Aminov <aminov@kailash-tech.jp>
2025-06-20 12:22:15 +0900

    momentを修正

--- src/intro.js
+++ src/intro.js
@@ -5,6 +5,20 @@
  */
 
 (function(factory) {
-  // jQueryは外部ファイルから取得
-  factory(jQuery, moment);
+  // jQueryは外部ファイルから取得、momentは内部から取得
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['jquery'], function(jquery) {
+      var moment = require('moment');
+      return factory(jquery, moment);
+    });
+  } else if (typeof exports === 'object') {
+    // CommonJS
+    var jquery = require('jquery');
+    var moment = require('moment');
+    module.exports = factory(jquery, moment);
+  } else {
+    // ブラウザグローバル
+    factory(jQuery, window.moment);
+  }
 })(function($, moment) {
